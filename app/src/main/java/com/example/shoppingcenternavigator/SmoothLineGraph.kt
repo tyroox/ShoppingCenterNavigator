cca73a81a1e9b07156539fecbf9d3c353d67b681
tyroox <atakanaktas99@hotmail.com>
2023-05-17 22:06:52 +0300

    Update

--- app/src/main/java/com/example/shoppingcenternavigator/SmoothLineGraph.kt
+++ app/src/main/java/com/example/shoppingcenternavigator/SmoothLineGraph.kt
@@ -1,6 +1,7 @@
 package com.example.shoppingcenternavigator
 
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
@@ -10,6 +11,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -88,6 +90,35 @@ fun distance(x1: Float, x2: Float, y1: Float, y2: Float): Float {
     return sqrt((x2 - x1).pow(2) + (y2 - y1).pow(2))
 }
 
+fun closestPoints(x: Float, y: Float, list:List<Point>): MutableList<Float> {
+    var closest1 = 1414.2136f
+    var closestPoint1 = Coordinate(0f,0f)
+    var closest2 = 1414.2136f
+    var closestPoint2 = Coordinate(0f,0f)
+
+    list.forEachIndexed{ _, points ->
+        if ((distance(x, points.x, y, points.y) < closest1) and (distance(x, points.x, y, points.y) < closest2)) {
+            closest2 = closest1
+            closestPoint2 = closestPoint1
+            closest1 = distance(x, points.x, y, points.y)
+            closestPoint1 = Coordinate(points.x, points.y)
+        }
+        else if ((distance(x, points.x, y, points.y) > closest1) and (distance(x, points.x, y, points.y) < closest2)){
+            closest2 = (distance(x, points.x, y, points.y))
+            closestPoint2 = Coordinate(points.x, points.y)
+        }
+    }
+    val returnList = mutableListOf<Float>()
+    returnList.add(closestPoint1.x)
+    returnList.add(closestPoint1.y)
+    returnList.add(closestPoint2.x)
+    returnList.add(closestPoint2.y)
+    returnList.add(closest1)
+    returnList.add(closest2)
+
+    return returnList
+}
+
 fun generatePath(shop: List<Shops>, point: List<Point>, data: List<Coordinate>, size: Size): Path {
     val path = Path()
 
@@ -100,7 +131,7 @@ fun generatePath(shop: List<Shops>, point: List<Point>, data: List<Coordinate>, 
     val widthPerCoordinate = size.width / rangeX
     val heightPerCoordinate = size.height / rangeY
 
-
+    /*
     shop.forEachIndexed { i, coordinate ->
         if (i == 0) {
             path.moveTo(
@@ -111,13 +142,60 @@ fun generatePath(shop: List<Shops>, point: List<Point>, data: List<Coordinate>, 
             )
         }
 
+
+
         val coordinateX = (coordinate.x - xMin.x) *
                 widthPerCoordinate
 
         val coordinateY = (coordinate.y - yMin.y) *
                 heightPerCoordinate
-        path.lineTo(coordinateX, coordinateY)
+
+
     }
+
+     */
+    path.moveTo(shops[0].x * widthPerCoordinate, shops[0].y * heightPerCoordinate)
+    path.lineTo(prime[0].x * widthPerCoordinate, prime[0].y * heightPerCoordinate)
+
+    var distance = 1414.2136f
+    var nextPoint = Coordinate(0f,0f)
+    var lastPoint = Coordinate(0f,0f)
+
+    distance = distance(prime[1].x, closestPoints(prime[0].x,prime[0].y,points)[0] ,prime[1].y, closestPoints(prime[0].x,prime[0].y,points)[1])
+    lastPoint = Coordinate(prime[0].x, prime[0].y)
+    nextPoint = Coordinate(closestPoints(prime[0].x,prime[0].y,points)[0], closestPoints(prime[0].x,prime[0].y,points)[1])
+
+    if (distance < distance(prime[1].x, closestPoints(prime[0].x,prime[0].y,points)[2] ,prime[1].y, closestPoints(prime[0].x,prime[0].y,points)[3])){
+        distance = distance(prime[1].x, closestPoints(prime[0].x,prime[0].y,points)[2] ,prime[1].y, closestPoints(prime[0].x,prime[0].y,points)[3])
+        nextPoint = Coordinate(closestPoints(prime[0].x,prime[0].y,points)[2], closestPoints(prime[0].x,prime[0].y,points)[3])
+    }
+    path.lineTo(nextPoint.x * widthPerCoordinate,nextPoint.y * heightPerCoordinate)
+
+    while (!((nextPoint.x == prime[1].x) and (nextPoint.y == prime[1].y))){
+
+        distance = distance(prime[1].x, closestPoints(nextPoint.x,nextPoint.y,points)[0] ,prime[1].y, closestPoints(nextPoint.x,nextPoint.y,points)[1])
+        lastPoint = Coordinate(nextPoint.x, nextPoint.y)
+
+        if (lastPoint != nextPoint){
+            nextPoint = Coordinate(closestPoints(prime[0].x,prime[0].y,points)[0], closestPoints(prime[0].x,prime[0].y,points)[1])
+
+            if (distance < distance(prime[1].x, closestPoints(nextPoint.x,nextPoint.y,points)[2] ,prime[1].y, closestPoints(nextPoint.x,nextPoint.y,points)[3])){
+                distance = distance(prime[1].x, closestPoints(nextPoint.x,nextPoint.y,points)[2] ,prime[1].y, closestPoints(nextPoint.x,nextPoint.y,points)[3])
+                nextPoint = Coordinate(closestPoints(nextPoint.x,nextPoint.y,points)[2], closestPoints(nextPoint.x,nextPoint.y,points)[3])
+            }
+
+
+            path.lineTo(nextPoint.x * widthPerCoordinate,nextPoint.y * heightPerCoordinate)
+        }
+        else{
+            Log.d("aa","aynı nokta")
+        }
+
+
+
+        break
+    }
+    path.lineTo(prime[1].x * widthPerCoordinate, prime[1].y * heightPerCoordinate)
     path.lineTo(shops[1].x * widthPerCoordinate, shops[1].y * heightPerCoordinate)
 
     return path
@@ -133,21 +211,25 @@ val points = listOf(
     Point(0,333f,467f),
     Point(0,333f,646f),
     Point(0,632f,467f),
-    Point(0,32f,646f),
+    Point(0,632f,646f),
     Point(0,830f,345f),
-    Point(0,798f,744f)
+    Point(0,798f,744f),
+    // primes
+    Point(0,333f,512f),
+    Point(0,632f,605f)
 )
 
 val shops = listOf(
     //Shops("Starbucks", 0, 836f,772f),
-    Shops("Kiğılı",0,661f,605f),
-    Shops("Mavi", 0,303f,512f)
+    Shops("Mavi", 0,303f,512f),
+    Shops("Kiğılı",0,661f,605f)
+
 )
 
 val prime = listOf(
     //Shops("Starbucks", 0, 836f,772f),
-    Shops("KiğılıPrime",0,632f,605f),
-    Shops("MaviPrime", 0,333f,512f)
+    Shops("MaviPrime", 0,333f,512f),
+    Shops("KiğılıPrime",0,632f,605f)
 )
 
 
